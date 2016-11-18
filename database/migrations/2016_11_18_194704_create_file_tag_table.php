ca8f506d9df45abdb72268ba55957e644af6613f
Gabriel Almeida <Gabriel Almeida>
2016-11-18 19:56:49 -0200

    PROJECT: Criando migrations inicias

---
+++ database/migrations/2016_11_18_194704_create_file_tag_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+          Schema::create('file_tag', function (Blueprint $table) {
+            $table->increments('id'); 
+            $table->integer('file_id'); 
+            $table->integer('tag_id'); 
+            $table->timestamps(); 
+
+            $table->foreign('file_id')->references('id')->on('files')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('file_tag'); 
+    }
+}
---
+++ database/migrations/2016_11_18_195048_create_tag_process_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagProcessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_process', function (Blueprint $table) {
+            $table->increments('id'); 
+            $table->integer('tag_id'); 
+            $table->integer('process_id'); 
+            $table->timestamps(); 
+
+            $table->foreign('tag_id')->references('id')->on('tags')
+            $table->foreign('process_id')->references('id')->on('processes'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tag_process'); 
+    }
+}
